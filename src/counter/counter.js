13f16515994dc9eac0a69b6bb4baeb85c65ae092
manavchawla11 <141018195+manavchawla11@users.noreply.github.com>
2023-07-31 16:59:00 +0530

    Added Decrement Counter

--- src/counter/counter.js
+++ src/counter/counter.js
@@ -1,9 +1,14 @@
 function incrementCount() {
   const textDiv = document.querySelector("#count");
   let numValue = Number.parseInt(textDiv.textContent);
-  console.log(textDiv);
   textDiv.textContent = ++numValue;
 }
+
+function decrementCount() {
+    const textDiv = document.querySelector("#count");
+    let numValue = Number.parseInt(textDiv.textContent);
+    textDiv.textContent = --numValue;
+  }
 
 function resetCount() {
   const textDiv = document.querySelector("#count");
